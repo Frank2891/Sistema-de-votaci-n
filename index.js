479ca6017766c9de2cf3a4831d89be3689d693aa
Frank Zambrano <frankz428@hotmail.com>
2020-12-21 05:37:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('SISTEMA DE VOTACION');
+console.log('A ROJO');
+console.log('B VERDE');
+console.log('C AZUL');
+
+let voto = prompt('digite su selecion');
+
+
+
+
+if( voto == 'A'){
+
+    console.log('usted voto por rojo');
+}else if( voto == 'B'){
+
+    console.log('usted voto por el verde');
+
+}else if ( voto == 'C'){
+    console.log('usted voto por azul');
+}else{
+    console.log('digito un dato incorrecto');
+}
